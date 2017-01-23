7476e70a420c481d2e7e9f59e38fef3b7591745c
Takafumi Iju <takafumi.iju@sk.com>
2017-01-23 14:27:46 +0900

    Modify top page.
    Add validator.

--- src/main/scala/controller/Controllers.scala
+++ src/main/scala/controller/Controllers.scala
@@ -12,6 +12,7 @@ object Controllers {
 
   object root extends RootController with Routes {
     val indexUrl = get("/?")(index).as('index)
+    val loginUrl = post("/login")(login).as('login)
   }
 
 }
--- src/main/scala/controller/RootController.scala
+++ src/main/scala/controller/RootController.scala
@@ -2,10 +2,27 @@ package controller
 
 import skinny._
 import skinny.controller.feature.ThymeleafTemplateEngineFeature
+import skinny.validator._
+import skinny.validator.required
 
 class RootController extends ApplicationController
-with ThymeleafTemplateEngineFeature {
+    with ThymeleafTemplateEngineFeature {
+
+  def requestParams = Params(params)
+
+  def validateParams = validation(
+    requestParams,
+    paramKey("userName") is required,
+    paramKey("password") is required
+  )
 
   def index = render("/root/index")
 
+  def login = {
+    if (validateParams.validate) {
+      render("/home/index")
+    } else {
+      render("/root/index")
+    }
+  }
 }
