22dcfee320af98e066753fc223554a11e02bfebf
Takafumi Iju <takafumi.iju@sk.com>
2017-01-23 12:49:14 +0900

    Modify top page.
    Change tab action from original code to Bootstrap code.

--- src/main/webapp/assets/js/index.js
+++ src/main/webapp/assets/js/index.js
@@ -1,15 +1,6 @@
 (function(){
    $("ul.nav.nav-tabs li a").on("click", function(e){
-       $("ul.nav.nav-tabs li").removeClass("active");
-       var li = $(this).parent();
-       $(li).addClass("active");
-
-       if ($(li).hasClass("sign-in")) {
-           $("#sign-in-context").show();
-           $("#sign-up-context").hide();
-       } else {
-           $("#sign-in-context").hide();
-           $("#sign-up-context").show();
-       }
+       e.preventDefault();
+       $(this).tab('show');
    });
 }).call(this);
