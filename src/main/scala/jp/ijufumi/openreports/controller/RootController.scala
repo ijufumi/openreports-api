3c67eb1886cc118fa6464b2d6a28e88a308dfafd
Takafumi Iju <takafumi.iju@sk.com>
2017-04-24 15:16:55 +0900

    Modify login process.

--- src/main/scala/jp/ijufumi/openreports/controller/RootController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/RootController.scala
@@ -3,7 +3,7 @@ package jp.ijufumi.openreports.controller
 import jp.ijufumi.openreports.model.Member
 import skinny._
 import skinny.controller.feature.ThymeleafTemplateEngineFeature
-import skinny.validator.{required, _}
+import skinny.validator.{ required, _ }
 
 class RootController extends ApplicationController
     with ThymeleafTemplateEngineFeature {
@@ -27,18 +27,24 @@ class RootController extends ApplicationController
   }
 
   def login = {
+    val userName = requestParams.getAs("userName").getOrElse("")
+    val password = requestParams.getAs("password").getOrElse("")
     if (validateParams.validate) {
-      val userName = requestParams.getAs("userName").getOrElse("")
-      val password = requestParams.getAs("password").getOrElse("")
-      val members : Seq[Member] = Member.where('emailAddress -> userName, 'password -> password).apply();
+      val members: Seq[Member] = Member.where('emailAddress -> userName, 'password -> password).apply();
 
       if (members.isEmpty) {
+        logger.info("invalid id or password : [" + userName + "][" + password + "]")
         set("userName", requestParams.getAs("userName").getOrElse(""))
         // TODO ログインエラーメッセージの設定
         render("/root/index")
+      } else {
+        skinnySession.setAttribute("memberInfo", userName);
+        redirect("/home/")
       }
-      skinnySession.setAttribute("memberInfo", userName);
-      redirect("/home/")
+    } else {
+      logger.info("invalid id or password : [" + userName + "][" + password + "]")
+      // TODO ログインエラーメッセージの設定
+      render("/root/index")
     }
   }
 }
