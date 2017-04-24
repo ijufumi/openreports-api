55c8c31959e5bd355c1cf0c5b1cf6f8e310ec093
Takafumi Iju <takafumi.iju@sk.com>
2017-04-24 15:16:33 +0900

    Modify field name.

--- src/main/scala/jp/ijufumi/openreports/model/Member.scala
+++ src/main/scala/jp/ijufumi/openreports/model/Member.scala
@@ -4,13 +4,13 @@ import org.joda.time.DateTime
 import scalikejdbc.{ ResultName, WrappedResultSet }
 import skinny.orm.{ SkinnyCRUDMapper, SkinnyMapper }
 
-case class Member(id: Long, emailAddress: String, password: String, createdAt: DateTime)
+case class Member(memberId: Long, emailAddress: String, password: String, createdAt: DateTime)
 
 object Member extends SkinnyCRUDMapper[Member] {
   override def tableName = "member"
   override def defaultAlias = createAlias("mem")
   override def extract(rs: WrappedResultSet, n: ResultName[Member]): Member = new Member(
-    id = rs.get(n.id),
+    memberId = rs.get(n.memberId),
     emailAddress = rs.get(n.emailAddress),
     password = rs.get(n.password),
     createdAt = rs.get(n.createdAt)
