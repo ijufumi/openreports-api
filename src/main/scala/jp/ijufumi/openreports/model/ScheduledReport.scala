d11d500a167c132d63b27be1fd903ef8facab1fe
Takafumi Iju <ijufumi@gmail.com>
2018-03-29 12:57:58 +0900

    typoを修正

--- src/main/scala/jp/ijufumi/openreports/model/ScheduledReport.scala
+++ src/main/scala/jp/ijufumi/openreports/model/ScheduledReport.scala
@@ -4,14 +4,14 @@ import org.joda.time.DateTime
 import scalikejdbc.WrappedResultSet
 import skinny.orm.SkinnyCRUDMapper
 
-case class ScheduledReport (scheduleId: Long, reportId: Long, cronExpression: String, createdAt: DateTime, updatedAt: DateTime)
+case class ScheduledReport(scheduleId: Long, reportId: Long, cronExpression: String, createdAt: DateTime, updatedAt: DateTime)
 
 object ScheduledReport extends SkinnyCRUDMapper[ScheduledReport] {
   override def tableName = "scheduled_report"
   override def defaultAlias = createAlias("sch_rep")
 
   override def extract(rs: WrappedResultSet, n: scalikejdbc.ResultName[ScheduledReport]): ScheduledReport = new ScheduledReport(
-    scheduleId = rs.get(n.scheduledId),
+    scheduleId = rs.get(n.scheduleId),
     reportId = rs.get(n.reportId),
     cronExpression = rs.get(n.cronExpression),
     createdAt = rs.get(n.createdAt),
