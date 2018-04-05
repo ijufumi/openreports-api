23962cf967bcaec349114242933b1fec0efbf6a7
Takafumi Iju <ijufumi@gmail.com>
2018-04-05 19:32:38 +0900

    Add request parameters.

--- src/main/scala/jp/ijufumi/openreports/controller/ReportController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/ReportController.scala
@@ -1,9 +1,13 @@
 package jp.ijufumi.openreports.controller
 
+import skinny.Params
 import skinny.controller.feature.ThymeleafTemplateEngineFeature
 
 class ReportController extends ApplicationController
   with ThymeleafTemplateEngineFeature {
+
+  def requestParams = Params(params)
+
   def index = {
     render("/report/index")
   }
