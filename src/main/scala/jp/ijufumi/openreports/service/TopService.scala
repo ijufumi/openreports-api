b07d6429127d5e26a16f00352ee5dfdb7c4d7112
ijufumi <ijufumi@gmail.com>
2018-06-20 08:36:13 +0900

    Add groupId to MemberInfo

--- src/main/scala/jp/ijufumi/openreports/service/TopService.scala
+++ src/main/scala/jp/ijufumi/openreports/service/TopService.scala
@@ -1,6 +1,6 @@
 package jp.ijufumi.openreports.service
 
-import jp.ijufumi.openreports.model.{ TGroup, TMember }
+import jp.ijufumi.openreports.model.{TGroup, TMember}
 import jp.ijufumi.openreports.vo.MemberInfo
 import skinny.LoggerProvider
 
@@ -15,14 +15,16 @@ class TopService extends LoggerProvider {
       logger.info("invalid id or password : [%s][%s]".format(emailAddress, password))
     } else {
       val menus = mutable.Set[Long]()
+      val groups = mutable.Set[Long]()
       val m = members.head
       for (g <- m.groups) {
         val group = TGroup.includes(TGroup.functions).findById(g.groupId).get
         logger.debug("Group:%s".format(group))
         menus ++= group.functions.map(_.functionId).toSet
+        groups + g.groupId
       }
 
-      member = MemberInfo(m.memberId, m.name, menus.toSet)
+      member = MemberInfo(m.memberId, m.name, groups.toSet, menus.toSet)
       logger.info("memberInfo:%s".format(member))
     }
     Option(member)
--- src/main/scala/jp/ijufumi/openreports/vo/MemberInfo.scala
+++ src/main/scala/jp/ijufumi/openreports/vo/MemberInfo.scala
@@ -2,4 +2,7 @@ package jp.ijufumi.openreports.vo
 
 import scala.collection.immutable.Set
 
-case class MemberInfo(memberId: Long, name: String, menus: Set[Long])
+case class MemberInfo(memberId: Long,
+                      name: String,
+                      groups: Set[Long],
+                      menus: Set[Long])
