4e06eab42e52e981c7db717f2d957972ad105b00
ijufumi <ijufumi@gmail.com>
2018-06-21 09:44:06 +0900

    [WIP] Show list of report groups.

--- src/main/scala/jp/ijufumi/openreports/service/ReportService.scala
+++ src/main/scala/jp/ijufumi/openreports/service/ReportService.scala
@@ -1,18 +1,24 @@
 package jp.ijufumi.openreports.service
 
 import jp.ijufumi.openreports.model.{ TGroup, TReportGroup }
+import skinny.logging.Logging
 
 import scala.collection.mutable
 
-class ReportService {
-  def list(groupId: Set[Long]): Set[TReportGroup] = {
-    val groups = TGroup.includes(TGroup.reportGroups).where('id -> groupId).apply()
+class ReportService extends Logging {
+  def list(groupId: Set[Long]): Seq[TReportGroup] = {
+    val groups = TGroup.includes(TGroup.reportGroups).where('groupId -> Seq(1, 2, 3)).apply() // TODO:Use groupId params.
+
+    logger.debug("groups:%s".format(groups))
+
     val reportGroups = mutable.Set[TReportGroup]()
     for (g <- groups; rg <- g.reportGroups) {
-      reportGroups + rg
+      reportGroups += rg
     }
 
-    reportGroups.toSet
+    logger.debug("reportGroups:%s".format(reportGroups))
+
+    reportGroups.toSeq.sortBy(_.reportGroupId)
   }
 }
 
