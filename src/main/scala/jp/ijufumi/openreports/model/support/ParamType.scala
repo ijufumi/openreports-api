f89a18f1ab5910ae7e932a676f39b957b78904c8
ijufumi <ijufumi@gmail.com>
2018-06-30 13:10:40 +0900

    Change case class to object with enum.

--- src/main/scala/jp/ijufumi/openreports/model/support/ParamType.scala
+++ src/main/scala/jp/ijufumi/openreports/model/support/ParamType.scala
@@ -1,8 +1,5 @@
 package jp.ijufumi.openreports.model.support
 
-case class ParamType(_type: String) {
-  val TEXT = ParamType("0")
-  val DATE = ParamType("1")
-  val LIST = ParamType("2")
-  val QUERY = ParamType("3")
+object ParamType extends Enumeration {
+  val TEXT, DATE, LIST, QUERY = Value
 }
