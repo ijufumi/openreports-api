9118bc99c57b4bfb8993644e1d88c38c9e620bf8
ijufumi <ijufumi@gmail.com>
2018-07-02 08:43:32 +0900

    Add implicit converter to ParamType.

--- src/main/scala/jp/ijufumi/openreports/model/support/ParamType.scala
+++ src/main/scala/jp/ijufumi/openreports/model/support/ParamType.scala
@@ -1,5 +1,17 @@
 package jp.ijufumi.openreports.model.support
 
 object ParamType extends Enumeration {
-  val TEXT, DATE, LIST, QUERY = Value
+
+  protected case class Val(value: String) extends super.Val
+
+  val TEXT = Value("1")
+  val DATE = Value("2")
+  val LIST = Value("3")
+  val QUERY = Value("4")
+
+  implicit def valueToVal(x: Value): Val = x.asInstanceOf[Val]
+
+  def of(value: String): ParamType.Val = {
+    values.find(_.value == value).getOrElse(TEXT)
+  }
 }
