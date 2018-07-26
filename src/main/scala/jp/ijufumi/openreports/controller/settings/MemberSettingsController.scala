1ebbf18d236cac6d87ef1ca5d3d9f4b1b298c1ba
ijufumi <ijufumi@gmail.com>
2018-07-26 17:59:36 +0900

    [WIP] Create managing functions of member.

--- src/main/scala/jp/ijufumi/openreports/controller/settings/MemberSettingsController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/MemberSettingsController.scala
@@ -1,13 +1,27 @@
 package jp.ijufumi.openreports.controller.settings
+
 import jp.ijufumi.openreports.controller.common.ApplicationController
 import jp.ijufumi.openreports.service.SettingMemberService
+import skinny.Params
+import skinny.validator.{email, paramKey, required}
 
-class MemberSettingsController extends ApplicationController {
+class MemberSettingsController
+  extends ApplicationController {
   val path = rootPath + "/member"
   val viewPath = rootPath + "/member"
 
   override val activeMenu = "settings/member"
   override val requiredMemberInfo = true
+
+  def requestParams = Params(params)
+
+  def validateRegisterParams = validation(
+    requestParams,
+    paramKey("name") is required,
+    paramKey("emailAddress") is required & email,
+    paramKey("password") is required,
+    paramKey("checkedPassword") is required
+  )
 
   def index = {
     val members = new SettingMemberService().getMembers()
@@ -20,7 +34,27 @@ class MemberSettingsController extends ApplicationController {
   }
 
   def register = {
-    redirect(path + "/registerCompleted")
+    if (validateRegisterParams.validate) {
+      val password = requestParams.getAs[String]("password").getOrElse("")
+      val checkedPassword = requestParams.getAs[String]("checkedPassword").getOrElse("")
+      // パスワードの不一致
+      if (!password.equals(checkedPassword)) {
+        logger.info("invalid params:%s".format(requestParams))
+        set("customErrorMessages", Seq(i18n.get("warning.loginFailure"))) // TODO:メッセージ変更
+        render(viewPath + "/form")
+      }
+      val name = requestParams.getAs[String]("name").getOrElse("")
+      val emailAddress = requestParams.getAs[String]("emailAddress").getOrElse("")
+      val isAdmin = requestParams.getAs[Boolean]("isAdmin").getOrElse(false)
+      new SettingMemberService()
+        .registerMember(name = name, emailAddress = emailAddress, password = password, isAdmin = isAdmin)
+      // TODO:emailAddressの重複チェックとエラー処理の追加
+      redirect(path + "/registerCompleted")
+    } else {
+      logger.info("invalid params:%s".format(requestParams))
+      set("customErrorMessages", Seq(i18n.get("warning.loginFailure"))) // TODO:メッセージ変更
+      render(viewPath + "/form")
+    }
   }
 
   def registerCompleted = {
