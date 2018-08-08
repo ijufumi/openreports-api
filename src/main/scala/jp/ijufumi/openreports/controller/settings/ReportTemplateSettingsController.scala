48b8b8f1425eddfa7011f2d38228374e8be3f042
ijufumi <ijufumi@gmail.com>
2018-08-08 13:55:58 +0900

    Add template-file upload function.

--- src/main/scala/jp/ijufumi/openreports/controller/settings/ReportTemplateSettingsController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/ReportTemplateSettingsController.scala
@@ -1,8 +1,11 @@
 package jp.ijufumi.openreports.controller.settings
 import jp.ijufumi.openreports.controller.common.ApplicationController
 import jp.ijufumi.openreports.service.settings.ReportTemplateSettingsService
+import skinny.controller.feature.FileUploadFeature
 
-class ReportTemplateSettingsController extends ApplicationController {
+class ReportTemplateSettingsController
+    extends ApplicationController
+    with FileUploadFeature {
 
   val path = rootPath + "/report-template"
   val viewPath = rootPath + "/report-template"
@@ -17,4 +20,14 @@ class ReportTemplateSettingsController extends ApplicationController {
     render(viewPath + "/upload")
   }
 
+  def upload = {
+    fileParams.get("uploadFile") match {
+      case Some(file) =>
+        logger.info(new String(file.get()))
+        new ReportTemplateSettingsService().uploadFile(file)
+      case None => logger.info("file not found")
+    }
+
+    redirect(url(Controllers.reportTemplateSettings.showUploadUrl))
+  }
 }
