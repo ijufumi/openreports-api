6676dfa351452438debfbde2ea8a4eec4b2f1df9
ijufumi <ijufumi@gmail.com>
2018-08-13 13:07:33 +0900

    Add register report function.

--- src/main/scala/jp/ijufumi/openreports/controller/settings/ReportSettingsController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/ReportSettingsController.scala
@@ -1,7 +1,11 @@
 package jp.ijufumi.openreports.controller.settings
 
 import jp.ijufumi.openreports.controller.common.ApplicationController
-import jp.ijufumi.openreports.service.settings.ReportSettingsService
+import jp.ijufumi.openreports.service.settings.{
+  ReportParamSettingsService,
+  ReportSettingsService,
+  ReportTemplateSettingsService
+}
 import skinny.controller.Params
 
 class ReportSettingsController extends ApplicationController {
@@ -9,15 +13,20 @@ class ReportSettingsController extends ApplicationController {
   val path = rootPath + "/report"
   val viewPath = rootPath + "/report"
 
+  def requestParams = Params(params)
+
   def index = {
-    val reports = new ReportSettingsService().getReports()
+    val reports = new ReportSettingsService().getReports
     set("reports", reports)
     render(viewPath + "/index")
   }
 
   def showRegister = {
+    val templates = new ReportTemplateSettingsService().getReportTemplates
+    val params = new ReportParamSettingsService().getParams
+
+    set("templates", templates)
+    set("params", params)
     render(viewPath + "/register")
   }
-
-  def requestParams = Params(params)
 }
