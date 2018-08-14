9de486e7692ae1c4b371af74d2b8be2faf878f74
ijufumi <ijufumi@gmail.com>
2018-08-14 17:12:21 +0900

    Add register/update function(controller)

--- src/main/scala/jp/ijufumi/openreports/controller/settings/Controllers.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/Controllers.scala
@@ -15,6 +15,21 @@ object Controllers {
 
   object reportSettings extends ReportSettingsController with Routes {
     val indexUrl = get(path + "/?")(index).as('index)
+
+    val showRegisterUrl =
+      get(path + "/register")(showRegister).as('showRegister)
+
+    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+
+    val registerCompletedUrl =
+      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+
+    val showRpdateUrl = get(path + "/update/:id")(showUpdate).as('showUpdate)
+
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+
+    val updateCompletedUrl =
+      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
   }
 
   object reportGroupSettings extends ReportGroupSettingsController with Routes {
--- src/main/scala/jp/ijufumi/openreports/controller/settings/ReportSettingsController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/ReportSettingsController.scala
@@ -1,12 +1,14 @@
 package jp.ijufumi.openreports.controller.settings
 
 import jp.ijufumi.openreports.controller.common.ApplicationController
+import jp.ijufumi.openreports.service.enums.StatusCode
 import jp.ijufumi.openreports.service.settings.{
   ReportParamSettingsService,
   ReportSettingsService,
   ReportTemplateSettingsService
 }
 import skinny.controller.Params
+import skinny.validator.{longValue, maxLength, paramKey, required}
 
 class ReportSettingsController extends ApplicationController {
 
@@ -14,6 +16,21 @@ class ReportSettingsController extends ApplicationController {
   val viewPath = rootPath + "/report"
 
   def requestParams = Params(params)
+
+  def validateRegisterParams = validation(
+    requestParams,
+    paramKey("reportName") is required & maxLength(250),
+    paramKey("description") is maxLength(250),
+    paramKey("templateId") is required
+  )
+
+  def validateUpdateParams = validation(
+    requestParams,
+    paramKey("reportName") is required & maxLength(250),
+    paramKey("description") is maxLength(250),
+    paramKey("templateId") is required,
+    paramKey("versions") is required & longValue
+  )
 
   def index = {
     val reports = new ReportSettingsService().getReports
@@ -28,5 +45,84 @@ class ReportSettingsController extends ApplicationController {
     set("templates", templates)
     set("params", params)
     render(viewPath + "/register")
+  }
+
+  def doRegister = {
+    if (validateRegisterParams.validate) {
+      val reportName = params.getAs[String]("reportName").get
+      val description = params.getAs[String]("description").getOrElse("")
+      val templateId = params.getAs[Long]("templateId").getOrElse(0)
+
+      val statusCode = new ReportSettingsService().registerReport(
+        reportName,
+        description,
+        templateId
+      )
+
+      statusCode match {
+        case StatusCode.OK => redirect(path + "/registerCompleted")
+        case StatusCode.DUPLICATE_ERR =>
+          set("customErrorMessages", Seq(i18n.get("warning.duplicateRegister")))
+        case _ =>
+          set("customErrorMessages", Seq(i18n.get("error.systemError")))
+      }
+      render(viewPath + "/register")
+
+    } else {
+      logger.info("invalid params:%s".format(params))
+      render(viewPath + "/register")
+    }
+  }
+
+  def registerCompleted = {
+    render(viewPath + "/register-completed")
+  }
+
+  def showUpdate = {
+    params.getAs[Long]("id").map { id =>
+      val reportOpt = new ReportSettingsService().getReport(id)
+      if (reportOpt.isEmpty) {
+        haltWithBody(404)
+      }
+      set("report", reportOpt.get)
+      render(viewPath + "/update")
+    } getOrElse haltWithBody(404)
+  }
+
+  def doUpdate = {
+    params.getAs[Long]("id").map { id =>
+      if (validateUpdateParams.validate) {
+        val reportName = params.getAs[String]("reportName").get
+        val description = params.getAs[String]("description").getOrElse("")
+        val templateId = params.getAs[Long]("templateId").getOrElse(0)
+        val versions = params.getAs[Long]("versions").getOrElse(0)
+
+        val statusCode = new ReportSettingsService().updateReport(
+          id,
+          reportName,
+          description,
+          templateId,
+          versions
+        )
+
+        statusCode match {
+          case StatusCode.OK => redirect(path + "/registerCompleted")
+          case StatusCode.DUPLICATE_ERR =>
+            set("customErrorMessages",
+                Seq(i18n.get("warning.duplicateRegister")))
+          case _ =>
+            set("customErrorMessages", Seq(i18n.get("error.systemError")))
+        }
+        render(viewPath + "/update")
+
+      } else {
+        logger.info("invalid params:%s".format(params))
+        render(viewPath + "/update")
+      }
+    } getOrElse haltWithBody(404)
+  }
+
+  def updateCompleted = {
+    render(viewPath + "/update-completed")
   }
 }
