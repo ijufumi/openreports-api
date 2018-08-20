677678bb05a118a3338d5a4c6c7a48a19dae5762
ijufumi <ijufumi@gmail.com>
2018-08-20 13:05:33 +0900

    Update password validator
    - Add rule of symbols
    - Change to be able to customize rules (minLength, number, alphabet, symbol)

--- src/main/scala/jp/ijufumi/openreports/controller/validator/CustomValidatorRules.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/validator/CustomValidatorRules.scala
@@ -2,11 +2,42 @@ package jp.ijufumi.openreports.controller.validator
 
 import skinny.validator.ValidationRule
 
-object password extends ValidationRule {
+object password extends password(10, true, true, true, true)
+
+case class password(
+    minLength: Int = 8,
+    needsNumber: Boolean = true,
+    needsLowerAlpha: Boolean = true,
+    needsUpperAlpha: Boolean = true,
+    needsSymbol: Boolean = true,
+) extends ValidationRule {
   def name = "password"
-  def isValid(value:Any):Boolean = isEmpty(value) || {
+  def isValid(value: Any): Boolean = isEmpty(value) || {
     val strValue = value.toString
-    strValue.length > 8 && strValue.matches(".*\d.*") &&
-      strValue.matches(".*[a-z].*") && strValue.matches(".*[A-Z].*")
+    strValue.length > minLength && {
+      if (needsNumber) {
+        strValue.matches(".*\\d.*")
+      } else {
+        true
+      }
+    } && {
+      if (needsLowerAlpha) {
+        strValue.matches(".*[a-z].*")
+      } else {
+        true
+      }
+    } && {
+      if (needsUpperAlpha) {
+        strValue.matches(".*[A-Z].*")
+      } else {
+        true
+      }
+    } && {
+      if (needsSymbol) {
+        strValue.matches(".*[!-/].*")
+      } else {
+        true
+      }
+    }
   }
 }
