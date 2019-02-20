0a8c4f49158eb458793629258264ed82471d6fc4
ijufumi <ijufumi@gmail.com>
2019-02-20 22:03:39 +0900

    Add feature that register relation of report and report-param.

--- src/main/webapp/assets/js/setting/report/report.js
+++ src/main/webapp/assets/js/setting/report/report.js
@@ -1,9 +1,17 @@
 $(function () {
   $(".private-setting-report-param-rel div.actions").each(function(idx){
-    if (idx === 0) {
+    if (idx === 1) {
       $("span:eq(1)", this).hide();
     } else {
-
+      $("span:eq(1)", this).on("click", function(e){
+        $(this).parent().parent().parent().remove();
+      });
     }
+    $("span:eq(0)", this).on("click", function(e){
+      var row = $("#tr-template").clone(true);
+      row.removeClass("is-hidden");
+      //$("#params").append(row);
+      $(this).parent().parent().parent().after(row);
+    });
   });
 });
