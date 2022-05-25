e816860b19ac816b48ffb2423a02b81e1aa855a1
Takafumi Iju <ijufumi@gmail.com>
2022-05-26 06:35:13 +0900

    Modify to use String instead of symbol.

--- src/main/scala/jp/ijufumi/openreports/controller/Controllers.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/Controllers.scala
@@ -13,30 +13,30 @@ object Controllers {
   }
 
   object top extends TopController with Routes {
-    val toTopUrl = get("/")(toTop).as('toTop)
-    val indexUrl = get(path)(index).as('index)
-    val indexUrl2 = get(path + "/login")(index).as('index)
-    val signUpUrl = get(path + "/signup")(signUp).as('signUp)
-    val loginUrl = post(path + "/login")(login).as('login)
+    val toTopUrl = get("/")(toTop).as("toTop")
+    val indexUrl = get(path)(index).as("index")
+    val indexUrl2 = get(path + "/login")(index).as("index")
+    val signUpUrl = get(path + "/signup")(signUp).as("signUp")
+    val loginUrl = post(path + "/login")(login).as("login")
   }
 
   object home extends HomeController with Routes {
-    val indexUrl = get(path)(index).as('index)
-    val logoutUrl = get(path + "/logout")(logout).as('logout)
+    val indexUrl = get(path)(index).as("index")
+    val logoutUrl = get(path + "/logout")(logout).as("logout")
   }
 
   object report extends ReportController with Routes {
-    val indexUrl = get(path)(groupList).as('index)
-    val reportListUrl = get(path + "/:id")(reportList).as('reportList)
-    val prepareUrl = get(path + "/prepare/:id")(prepareReport).as('prepare)
-    val paramsUrl = post(path + "/params")(setParams).as('setParams)
+    val indexUrl = get(path)(groupList).as("index")
+    val reportListUrl = get(path + "/:id")(reportList).as("reportList")
+    val prepareUrl = get(path + "/prepare/:id")(prepareReport).as("prepare")
+    val paramsUrl = post(path + "/params")(setParams).as("setParams")
     val printOutReportUrl =
-      get(path + "/printOut/:id")(printOutReport).as('printOutReport)
-    val downloadUrl = get(path + "/download")(download).as('download)
+      get(path + "/printOut/:id")(printOutReport).as("printOutReport")
+    val downloadUrl = get(path + "/download")(download).as("download")
   }
 
   object scheduledReport extends ScheduledReportController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
   }
 
 }
