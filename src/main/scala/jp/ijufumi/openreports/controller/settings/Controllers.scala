ffbee1b4a481a78a9b77cbda14000ce2dde67f9d
Takafumi Iju <ijufumi@gmail.com>
2022-05-29 08:03:38 +0900

    Change symbol to string.

--- src/main/scala/jp/ijufumi/openreports/controller/settings/Controllers.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/settings/Controllers.scala
@@ -15,115 +15,115 @@ object Controllers {
   }
 
   object reportSettings extends ReportSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
     val showRegisterUrl =
-      get(path + "/register")(showRegister).as('showRegister)
+      get(path + "/register")(showRegister).as("showRegister")
 
-    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+    val doRegisterUrl = post(path + "/register")(doRegister).as("doRegister")
 
     val registerCompletedUrl =
-      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+      get(path + "/registerCompleted")(registerCompleted).as("registerCompleted")
 
-    val showUpdateUrl = get(path + "/update/:id")(showUpdate).as('showUpdate)
+    val showUpdateUrl = get(path + "/update/:id")(showUpdate).as("showUpdate")
 
-    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as("doUpdate")
 
     val updateCompletedUrl =
-      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
+      get(path + "/updateCompleted")(updateCompleted).as("updateCompleted")
 
-    val showUpdateParamsUrl = get(path + "/params/:id")(showUpdateParams).as('showUpdateParams)
+    val showUpdateParamsUrl = get(path + "/params/:id")(showUpdateParams).as("showUpdateParams")
 
-    val doUpdateParamsUrl = post(path + "/params/:id")(doUpdateParams).as('doUpdateParams)
+    val doUpdateParamsUrl = post(path + "/params/:id")(doUpdateParams).as("doUpdateParams")
   }
 
   object reportGroupSettings extends ReportGroupSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
     val showRegisterUrl =
-      get(path + "/register")(showRegister).as('showRegister)
+      get(path + "/register")(showRegister).as("showRegister")
 
-    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+    val doRegisterUrl = post(path + "/register")(doRegister).as("doRegister")
 
     val registerCompletedUrl =
-      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+      get(path + "/registerCompleted")(registerCompleted).as("registerCompleted")
 
-    val showRpdateUrl = get(path + "/update/:id")(showUpdate).as('showUpdate)
+    val showRpdateUrl = get(path + "/update/:id")(showUpdate).as("showUpdate")
 
-    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as("doUpdate")
 
     val updateCompletedUrl =
-      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
+      get(path + "/updateCompleted")(updateCompleted).as("updateCompleted")
 
   }
 
   object reportTemplateSettings extends ReportTemplateSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
-    val showHistoryUrl = get(path + "/history/:id")(showHistory).as('showHistory)
+    val showHistoryUrl = get(path + "/history/:id")(showHistory).as("showHistory")
 
-    val showUploadUrl = get(path + "/upload")(showUpload).as('showUpload)
+    val showUploadUrl = get(path + "/upload")(showUpload).as("showUpload")
 
-    val uploaderUrl = post(path + "/upload")(upload).as('upload)
+    val uploaderUrl = post(path + "/upload")(upload).as("upload")
   }
 
   object reportParamSettings extends ReportParamSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
     val showRegisterUrl =
-      get(path + "/register")(showRegister).as('showRegister)
+      get(path + "/register")(showRegister).as("showRegister")
 
-    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+    val doRegisterUrl = post(path + "/register")(doRegister).as("doRegister")
 
     val registerCompletedUrl =
-      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+      get(path + "/registerCompleted")(registerCompleted).as("registerCompleted")
 
     val showUpdateUrl =
-      get(path + "/update/:id")(showUpdate).as('showUpdate)
+      get(path + "/update/:id")(showUpdate).as("showUpdate")
 
-    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as("doUpdate")
 
     val updateCompletedUrl =
-      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
+      get(path + "/updateCompleted")(updateCompleted).as("updateCompleted")
 
   }
 
   object memberSettings extends MemberSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
     val showRegisterUrl =
-      get(path + "/register")(showRegister).as('showRegister)
+      get(path + "/register")(showRegister).as("showRegister")
 
-    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+    val doRegisterUrl = post(path + "/register")(doRegister).as("doRegister")
 
     val registerCompletedUrl =
-      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+      get(path + "/registerCompleted")(registerCompleted).as("registerCompleted")
 
-    val showUpdateUrl = get(path + "/update/:id")(showUpdate).as('showUpdate)
+    val showUpdateUrl = get(path + "/update/:id")(showUpdate).as("showUpdate")
 
-    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as("doUpdate")
 
     val updateCompletedUrl =
-      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
+      get(path + "/updateCompleted")(updateCompleted).as("updateCompleted")
   }
 
   object groupSettings extends GroupSettingsController with Routes {
-    val indexUrl = get(path)(index).as('index)
+    val indexUrl = get(path)(index).as("index")
 
     val showRegisterUrl =
-      get(path + "/register")(showRegister).as('showRegister)
+      get(path + "/register")(showRegister).as("showRegister")
 
-    val doRegisterUrl = post(path + "/register")(doRegister).as('doRegister)
+    val doRegisterUrl = post(path + "/register")(doRegister).as("doRegister")
 
     val registerCompletedUrl =
-      get(path + "/registerCompleted")(registerCompleted).as('registerCompleted)
+      get(path + "/registerCompleted")(registerCompleted).as("registerCompleted")
 
-    val showRpdateUrl = get(path + "/update/:id")(showUpdate).as('showUpdate)
+    val showRpdateUrl = get(path + "/update/:id")(showUpdate).as("showUpdate")
 
-    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as('doUpdate)
+    val doUpdateUrl = post(path + "/update/:id")(doUpdate).as("doUpdate")
 
     val updateCompletedUrl =
-      get(path + "/updateCompleted")(updateCompleted).as('updateCompleted)
+      get(path + "/updateCompleted")(updateCompleted).as("updateCompleted")
   }
 
 }
