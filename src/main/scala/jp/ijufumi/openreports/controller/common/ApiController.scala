f50a1b78f936ef10ed7ea9ae76231b10c84cdfe2
Takafumi Iju <ijufumi@gmail.com>
2022-06-01 06:18:10 +0900

    Add necessary trait.

--- src/main/scala/jp/ijufumi/openreports/controller/common/ApiController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/common/ApiController.scala
@@ -1,11 +1,13 @@
 package jp.ijufumi.openreports.controller.common
 
+import jp.ijufumi.openreports.vo.ApiResponse
 import skinny._
+import skinny.controller.feature.JSONFeature
 
 /**
   * The base jp.ijufumi.openreports.controller for API endpoints.
   */
-trait ApiController extends SkinnyApiController {
+trait ApiController extends SkinnyApiController with JSONFeature with I18nFeature {
 
   /*
    * Handles when unexpected exceptions are thrown from controllers.
@@ -17,4 +19,11 @@ trait ApiController extends SkinnyApiController {
       haltWithBody(500)
   }
 
+  def okResponse(entity: Any): String = {
+    responseAsJSON(ApiResponse(message = entity))
+  }
+
+  def ngResponse(message: Option[String]): String = {
+    responseAsJSON(ApiResponse(status = false, message = message.getOrElse("error")))
+  }
 }
