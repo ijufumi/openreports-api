947a510aec9e2faf98dfccbd69194b84cfdd1e4d
Takafumi Iju <ijufumi@gmail.com>
2022-06-01 06:18:24 +0900

    Modify process

--- src/main/scala/jp/ijufumi/openreports/controller/TopController.scala
+++ src/main/scala/jp/ijufumi/openreports/controller/TopController.scala
@@ -1,57 +1,33 @@
 package jp.ijufumi.openreports.controller
 
-import jp.ijufumi.openreports.controller.common.ApplicationController
+import jp.ijufumi.openreports.controller.common.ApiController
 import jp.ijufumi.openreports.service.TopService
 import skinny._
 import skinny.validator.{required, _}
 
-class TopController extends ApplicationController {
+class TopController extends ApiController {
 
-  override val activeMenu = "top"
-  val path = PublicPath
-  val viewPath = PublicPath + "/top"
+  val path: String = PublicPath
 
-  def toTop = redirect(url(Controllers.top.indexUrl))
-
-  def index = {
-    //render("/top/index")
-    val memberInfo: Option[Any] = skinnySession.getAttribute("memberInfo")
-    if (memberInfo.isDefined) {
-      redirect(url(Controllers.home.indexUrl))
-    } else {
-      render(viewPath + "/index")
-    }
-  }
-
-  def signUp = {
-    render(viewPath + "/sign-up")
-  }
-
-  def login = {
+  def login: String = {
     val loginId = requestParams.getAs("loginId").getOrElse("")
     val password = requestParams.getAs("password").getOrElse("")
     if (validateParams.validate()) {
       val memberInfoOpt = new TopService().login(loginId, password)
 
       if (memberInfoOpt.isEmpty) {
-        set("loginId", requestParams.getAs("loginId").getOrElse(""))
-        set("customErrorMessages", Seq(i18n.get("warning.loginFailure")))
-        render(viewPath + "/index")
+        ngResponse(i18n.get("warning.loginFailure"))
       } else {
-        skinnySession.setAttribute("memberInfo", memberInfoOpt.get);
-        redirect(url(Controllers.home.indexUrl))
+        okResponse(memberInfoOpt.get)
       }
     } else {
-      logger.info(
-        "invalid id or password : [" + loginId + "][" + password + "]"
-      )
-      render(viewPath + "/index")
+      ngResponse(i18n.get("warning.loginFailure"))
     }
   }
 
   def requestParams = Params(params)
 
-  def validateParams = validation(
+  def validateParams: MapValidator = validation(
     requestParams,
     paramKey("loginId") is required,
     paramKey("password") is required
