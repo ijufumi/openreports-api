1434335c44ed6b840dcfa32d68e107ac2d01bf0e
Takafumi Iju <ijufumi@gmail.com>
2022-06-13 06:59:56 +0900

    Add launcher class

---
+++ src/main/scala/JettyLauncher.scala
@@ -0,0 +1,24 @@
+import sys.env
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
+
+object JettyLauncher {
+  def main(args: Array[String]) {
+    val port = sys.env.getOrElse("PORT", "8080").toInt
+
+    val server = new Server(port)
+    val context = new WebAppContext()
+    context setContextPath "/"
+    context.setResourceBase("src/main/webapp")
+    context.setInitParameter(ScalatraListener.LifeCycleKey, "ScalatraBootstrap")
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
+
+    server.setHandler(context)
+
+    server.start
+    server.join
+  }
+}
