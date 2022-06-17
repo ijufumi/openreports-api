3ba341d503b703ed9ef3be1dc084450be6c1fda0
Takafumi Iju <ijufumi@gmail.com>
2022-06-18 06:56:24 +0900

    Add keys

--- liquibasePlugin/src/main/scala/LiquibasePlugin.scala
+++ liquibasePlugin/src/main/scala/LiquibasePlugin.scala
@@ -1,7 +1,17 @@
 import sbt._
 import liquibase.integration.commandline.LiquibaseCommandLine
 
+object Import {
+  // settings
+  val liquibaseChangeLogFile = SettingKey[String]("Specifies the root changelog")
+  val liquibaseUrl = SettingKey[String]("Specifies the JDBC database connection URL")
+  val liquibaseUsername = SettingKey[String]("Specifies the database username")
+  val liquibasePassword = SettingKey[String]("Specifies the database password")
+  // tasks
+  val liquibaseUpdate = TaskKey[Unit]("liquibase-update", "Run a liquibase migration")
+}
+
 object LiquibasePlugin extends AutoPlugin {
-  object autoImport {}
+  import Import._
 
 }
