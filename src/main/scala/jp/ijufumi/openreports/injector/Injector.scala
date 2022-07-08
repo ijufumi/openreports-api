e2d39219d3562b2a2331ddbcd9134b044004b5c8
Takafumi Iju <ijufumi@gmail.com>
2022-07-09 07:34:42 +0900

    Remove duplicated code.

--- src/main/scala/jp/ijufumi/openreports/injector/Injector.scala
+++ src/main/scala/jp/ijufumi/openreports/injector/Injector.scala
@@ -3,13 +3,13 @@ package jp.ijufumi.openreports.injector
 import com.google.inject.Guice
 
 object Injector {
+  private val injector = Guice.createInjector(new DatabaseModule(), new ServiceModule())
+
   def createAndInject[T](clazz: Class[_ <: T]): T = {
-    val injector = Guice.createInjector(new DatabaseModule(), new ServiceModule())
     injector.getInstance(clazz)
   }
 
   def inject(instance: AnyRef): Unit = {
-    val injector = Guice.createInjector(new DatabaseModule(), new ServiceModule())
     injector.injectMembers(instance)
   }
 }
