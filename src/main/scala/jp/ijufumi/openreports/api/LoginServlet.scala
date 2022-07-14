917f37827d5da7534a92674b02cd911919404f57
Takafumi Iju <ijufumi@gmail.com>
2022-07-15 08:50:15 +0900

    Add implementation for login

--- src/main/scala/jp/ijufumi/openreports/api/LoginServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/LoginServlet.scala
@@ -8,7 +8,11 @@ import org.scalatra._
 class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase {
   post("/") {
     val request = parsedBody.extract[LoginRequest]
-    loginService.login(request.email, request.password)
-    Ok("hello")
+    val member = loginService.login(request.email, request.password)
+    if (member.isEmpty) {
+      Unauthorized("email or password or both are incorrect")
+    } else {
+      Ok(member.get)
+    }
   }
 }
