26c87e8ef484eb2cc9337530fb3be5c8d408034d
Takafumi Iju <ijufumi@gmail.com>
2022-07-15 09:50:17 +0900

    Fixed error of parseBody

--- src/main/scala/jp/ijufumi/openreports/api/APIServletBase.scala
+++ src/main/scala/jp/ijufumi/openreports/api/APIServletBase.scala
@@ -6,4 +6,10 @@ import org.scalatra.json.JacksonJsonSupport
 
 abstract class APIServletBase extends ScalatraServlet with JacksonJsonSupport {
   override protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
+
+  def extractBody[T] = parse(request.body).extract[T]
 }
--- src/main/scala/jp/ijufumi/openreports/api/LoginServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/LoginServlet.scala
@@ -7,8 +7,8 @@ import org.scalatra._
 
 class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase {
   post("/") {
-    val request = parsedBody.extract[LoginRequest]
-    val member = loginService.login(request.email, request.password)
+    val loginRequest = extractBody[LoginRequest]
+    val member = loginService.login(loginRequest.email, loginRequest.password)
     if (member.isEmpty) {
       Unauthorized("email or password or both are incorrect")
     } else {
