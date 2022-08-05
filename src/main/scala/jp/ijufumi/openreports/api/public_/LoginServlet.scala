6241a388ddcf98085d50245b1f214fa865d98d8a
Takafumi Iju <ijufumi@gmail.com>
2022-08-06 08:00:20 +0900

    Change path

--- src/main/scala/jp/ijufumi/openreports/api/public_/LoginServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/public_/LoginServlet.scala
@@ -7,7 +7,7 @@ import jp.ijufumi.openreports.vo.request.{GoogleLoginRequest, LoginRequest}
 import org.scalatra._
 
 class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase {
-  post("/") {
+  post("/password") {
     val loginRequest = extractBody[LoginRequest]()
     val member = loginService.login(loginRequest.email, loginRequest.password)
     if (member.isEmpty) {
