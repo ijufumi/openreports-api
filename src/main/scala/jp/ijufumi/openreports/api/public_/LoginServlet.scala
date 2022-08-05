6e272ed8426c5adebd36d68d32f12c371c95c41d
Takafumi Iju <ijufumi@gmail.com>
2022-08-06 07:20:08 +0900

    Consolidate login features

--- src/main/scala/jp/ijufumi/openreports/api/public_/LoginServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/public_/LoginServlet.scala
@@ -3,15 +3,28 @@ package jp.ijufumi.openreports.api.public_
 import com.google.inject.Inject
 import jp.ijufumi.openreports.api.base.APIServletBase
 import jp.ijufumi.openreports.services.LoginService
-import jp.ijufumi.openreports.vo.request.LoginRequest
+import jp.ijufumi.openreports.vo.request.{GoogleLoginRequest, LoginRequest}
 import org.scalatra._
 
 class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase {
-  post("/login") {
+  post("/") {
     val loginRequest = extractBody[LoginRequest]()
     val member = loginService.login(loginRequest.email, loginRequest.password)
     if (member.isEmpty) {
       Unauthorized("email or password or both are incorrect")
+    } else {
+      Ok(member.get)
+    }
+  }
+  get("/google/authorization_url") {
+    loginService.getAuthorizationUrl
+  }
+
+  post("/google") {
+    val loginRequest = extractBody[GoogleLoginRequest]()
+    val member = loginService.loginWithGoogle(loginRequest.state, loginRequest.code)
+    if (member.isEmpty) {
+      Unauthorized("state or code or both are invalid")
     } else {
       Ok(member.get)
     }
---
+++ src/main/scala/jp/ijufumi/openreports/vo/request/GoogleLoginRequest.scala
@@ -0,0 +1,3 @@
+package jp.ijufumi.openreports.vo.request
+
+case class GoogleLoginRequest (state: String, code: String)
