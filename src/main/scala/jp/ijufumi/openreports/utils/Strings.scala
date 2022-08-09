28520b439c3d296cae617ecfef375ec797099ded
Takafumi Iju <ijufumi@gmail.com>
2022-08-10 06:56:48 +0900

    Fixed bug of url string

--- src/main/scala/jp/ijufumi/openreports/utils/Strings.scala
+++ src/main/scala/jp/ijufumi/openreports/utils/Strings.scala
@@ -4,11 +4,14 @@ import scala.collection.mutable
 import scala.util.Random
 import scala.collection.mutable.ArrayBuffer
 import org.apache.commons.codec.binary.Base64
+import org.apache.commons.codec.net.URLCodec
 
 object Strings {
   private val LOWER_CASE = "abcdefghijklmnopqrstuvwxyz"
   private val UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   private val NUMERIC = "0123456789"
+
+  private val urlCodec = new URLCodec()
 
   def generateRandomSting(
       count: Int,
@@ -37,10 +40,15 @@ object Strings {
     baseArray.mkString
   }
 
-  def convertFromMap(values: mutable.Map[String, Any]): String = {
+  def generateQueryParamsFromMap(values: mutable.Map[String, Any]): String = {
     val builder = new mutable.StringBuilder
+    var index = 0
     values.keys.foreach(k => {
-      builder + s"${k}=${values.get(k)}"
+      builder ++= s"${k}=${urlCodec.encode(values.getOrElse(k, ""))}"
+      if (index != values.size - 1) {
+        builder ++= "&"
+      }
+      index += 1
     })
     builder.mkString
   }
