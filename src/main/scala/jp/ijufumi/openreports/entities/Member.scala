7ed7a3f2c045e68da9a910a1ba51bc0a3c852b3f
Takafumi Iju <ijufumi@gmail.com>
2022-08-14 16:31:14 +0900

    Modify entity.

--- src/main/scala/jp/ijufumi/openreports/entities/Member.scala
+++ src/main/scala/jp/ijufumi/openreports/entities/Member.scala
@@ -1,6 +1,7 @@
 package jp.ijufumi.openreports.entities
 
 import slick.jdbc.PostgresProfile.api._
+import slick.lifted.ProvenShape
 
 import java.sql.Timestamp
 import java.time.LocalDateTime
@@ -22,17 +23,17 @@ class Members(tag: Tag)
       tag,
       "members",
     ) {
-  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-  def googleId = column[String]("google_id", O.Unique)
-  def emailAddress = column[String]("email_address", O.Unique)
-  def password = column[String]("password")
-  def name = column[String]("name")
-  def isAdmin = column[String]("is_admin")
-  def createdAt = column[Timestamp]("created_at")(timestampType)
-  def updatedAt = column[Timestamp]("updated_at")(timestampType)
-  def version = column[Long]("version")
+  def id: Rep[Int] = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def googleId: Rep[String] = column[String]("google_id", O.Unique)
+  def emailAddress: Rep[String] = column[String]("email_address", O.Unique)
+  def password: Rep[String] = column[String]("password")
+  def name: Rep[String] = column[String]("name")
+  def isAdmin: Rep[String] = column[String]("is_admin")
+  def createdAt: Rep[Timestamp] = column[Timestamp]("created_at")(timestampType)
+  def updatedAt: Rep[Timestamp] = column[Timestamp]("updated_at")(timestampType)
+  def version: Rep[Long] = column[Long]("version")
 
-  def * =
+  override def * : ProvenShape[Member] =
     (
       id.?,
       googleId,
