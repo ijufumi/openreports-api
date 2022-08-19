bab656155cc38e0c338cd09c6758a724170cb4f7
Takafumi Iju <ijufumi@gmail.com>
2022-08-20 08:44:23 +0900

    Fixed test data

--- src/main/scala/db/migration/V000001_01__Create_user.scala
+++ src/main/scala/db/migration/V000001_01__Create_user.scala
@@ -6,20 +6,18 @@ import jp.ijufumi.openreports.utils.Hash
 
 class V000001_01__Create_user extends BaseJavaMigration {
   override def migrate(context: Context): Unit = {
-    try {
-      val email = "root@jufumi.jp"
-      val password = Hash.hmacSha256("password")
-      val name = "Root User"
-      val statement: java.sql.PreparedStatement = {
-        context.getConnection.prepareStatement(
-          s"INSERT INTO members (email, password, name) VALUES (?, ?, ?)",
-        )
-      }
-      statement.setString(1, email)
-      statement.setString(2, password)
-      statement.setString(3, name)
-      try statement.execute
-      finally if (statement != null) statement.close()
+    val email = "root@jufumi.jp"
+    val password = Hash.hmacSha256("password")
+    val name = "Root User"
+    val statement: java.sql.PreparedStatement = {
+      context.getConnection.prepareStatement(
+        s"INSERT INTO members (email, password, name) VALUES (?, ?, ?)",
+      )
     }
+    statement.setString(1, email)
+    statement.setString(2, password)
+    statement.setString(3, name)
+    try statement.execute
+    finally if (statement != null) statement.close()
   }
 }
