52538c683ededa5106ca5e626f3433f92be0f138
Takafumi Iju <ijufumi@gmail.com>
2022-08-20 08:44:46 +0900

    Create common feature

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,4 +1,3 @@
-import jp.ijufumi.openreports.api._
 import jp.ijufumi.openreports.api.public_.{HealthServlet, LoginServlet}
 import org.scalatra._
 
@@ -7,10 +6,16 @@ import jp.ijufumi.openreports.injector.Injector
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    val apiServlet = new HealthServlet
-    Injector.inject(apiServlet)
-    val loginServlet = Injector.createAndInject[ScalatraServlet](classOf[LoginServlet])
-    context.mount(loginServlet, "/login")
-    context.mount(apiServlet, "/*")
+    mount(context, classOf[HealthServlet], "/health")
+    mount(context, classOf[LoginServlet], "/login")
+  }
+
+  def mount(
+      context: ServletContext,
+      servletClass: Class[_ <: ScalatraServlet],
+      uriPattern: String,
+  ): Unit = {
+    def servlet = Injector.createAndInject[ScalatraServlet](servletClass)
+    context.mount(servlet, uriPattern)
   }
 }
