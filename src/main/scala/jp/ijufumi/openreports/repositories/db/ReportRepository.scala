e9e0152fab80f1445d338872c8be8c13629355de
Takafumi Iju <ijufumi@gmail.com>
2022-09-17 08:15:10 +0900

    Add joining of template

--- src/main/scala/jp/ijufumi/openreports/repositories/db/ReportRepository.scala
+++ src/main/scala/jp/ijufumi/openreports/repositories/db/ReportRepository.scala
@@ -1,10 +1,12 @@
 package jp.ijufumi.openreports.repositories.db
 
-import jp.ijufumi.openreports.entities.Report
+import jp.ijufumi.openreports.entities.{Report, ReportTemplate}
 
 trait ReportRepository {
 
   def getById(id: String): Option[Report]
+
+  def getWithTemplateById(id: String): Option[(Report, ReportTemplate)]
 
   def register(model: Report): Option[Report]
 
--- src/main/scala/jp/ijufumi/openreports/repositories/db/impl/ReportRepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/repositories/db/impl/ReportRepositoryImpl.scala
@@ -1,8 +1,8 @@
 package jp.ijufumi.openreports.repositories.db.impl
 
 import com.google.inject.Inject
-import jp.ijufumi.openreports.entities.Report
-import jp.ijufumi.openreports.entities.queries.{reportQuery => query}
+import jp.ijufumi.openreports.entities.{Report, ReportTemplate}
+import jp.ijufumi.openreports.entities.queries.{reportQuery => query, reportTemplateQuery}
 import slick.jdbc.PostgresProfile.api._
 import jp.ijufumi.openreports.repositories.db.ReportRepository
 import slick.jdbc.JdbcBackend.Database
@@ -18,8 +18,19 @@ class ReportRepositoryImpl @Inject() (db: Database) extends ReportRepository {
     if (models.isEmpty) {
       return None
     }
-    Option(models.head)
+    Some(models.head)
+  }
 
+  override def getWithTemplateById(id: String): Option[(Report, ReportTemplate)] = {
+    val getById = query
+      .filter(_.id === id)
+      .join(reportTemplateQuery)
+      .on(_.reportTemplateId == _.id)
+    val models = Await.result(db.run(getById.result), Duration("10s"))
+    if (models.isEmpty) {
+      return None
+    }
+    Some(models.head)
   }
 
   override def register(model: Report): Option[Report] = {
