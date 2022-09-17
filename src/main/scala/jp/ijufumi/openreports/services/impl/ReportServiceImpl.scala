1dc0786c203bf03b1ceb540d5879f63331b24f1d
Takafumi Iju <ijufumi@gmail.com>
2022-09-17 09:25:08 +0900

    Fixed build error.

--- src/main/scala/jp/ijufumi/openreports/services/impl/ReportServiceImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/services/impl/ReportServiceImpl.scala
@@ -2,4 +2,6 @@ package jp.ijufumi.openreports.services.impl
 
 import jp.ijufumi.openreports.services.ReportService
 
-class ReportServiceImpl extends ReportService {}
+class ReportServiceImpl extends ReportService {
+  def getReports(): Unit = {}
+}
