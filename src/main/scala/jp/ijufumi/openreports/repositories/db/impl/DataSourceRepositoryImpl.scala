32f50c9e986a835b37177f2ac339c2f10bbd0da4
Takafumi Iju <ijufumi@gmail.com>
2022-09-23 10:42:31 +0900

    Fixed build error

--- src/main/scala/jp/ijufumi/openreports/repositories/db/impl/DataSourceRepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/repositories/db/impl/DataSourceRepositoryImpl.scala
@@ -2,7 +2,7 @@ package jp.ijufumi.openreports.repositories.db.impl
 
 import com.google.inject.Inject
 import jp.ijufumi.openreports.entities.{DataSource, DriverType}
-import jp.ijufumi.openreports.entities.queries.{driverTypeQuery, dataSourceQuery => query}
+import jp.ijufumi.openreports.entities.queries.{dataSourceQuery => query, driverTypeQuery}
 import jp.ijufumi.openreports.repositories.db.DataSourceRepository
 import slick.jdbc.JdbcBackend.Database
 import slick.jdbc.PostgresProfile.api._
@@ -14,7 +14,7 @@ class DataSourceRepositoryImpl @Inject() (db: Database) extends DataSourceReposi
   override def getById(id: String): Option[(DataSource, DriverType)] = {
     val getDataSources = query
       .join(driverTypeQuery)
-      .on(_.driver_type_id === _.id)
+      .on(_.driverTypeId === _.id)
       .filter(_._1.id === id)
     val dataSources = Await.result(db.run(getDataSources.result), Duration("10s"))
     if (dataSources.isEmpty) {
