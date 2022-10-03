cfe3d7be79a263f1d5042638bdf6e3d10c7fde1c
Takafumi Iju <ijufumi@gmail.com>
2022-10-04 07:57:28 +0900

    Apply formatter

--- src/main/scala/jp/ijufumi/openreports/api/private_/MembersServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/private_/MembersServlet.scala
@@ -5,7 +5,7 @@ import jp.ijufumi.openreports.api.base.PrivateAPIServletBase
 import jp.ijufumi.openreports.services.LoginService
 import org.scalatra._
 
-class MembersServlet @Inject()(loginService: LoginService)
+class MembersServlet @Inject() (loginService: LoginService)
     extends PrivateAPIServletBase(loginService) {
   get("/status") {
     val apiToken = getApiToken()
