6f2c614053ae9e0d83fa81fc6039e25276eb9108
Takafumi Iju <ijufumi@gmail.com>
2022-10-11 09:15:28 +0900

    Fixed error of scala 2.12

--- src/main/scala/jp/ijufumi/openreports/database/DatabaseFactory.scala
+++ src/main/scala/jp/ijufumi/openreports/database/DatabaseFactory.scala
@@ -3,25 +3,27 @@ package jp.ijufumi.openreports.database
 import com.typesafe.config.ConfigFactory
 import slick.jdbc.JdbcBackend.Database
 
-import scala.jdk.CollectionConverters.MapHasAsJava
+import scala.jdk.CollectionConverters.mapAsJavaMap
 import jp.ijufumi.openreports.config.Config._
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
 object DatabaseFactory {
   private val config = ConfigFactory.parseMap(
-    Map(
-      "postgres" -> Map(
-        "url" -> f"jdbc:postgresql://$DB_HOST%s:$DB_PORT%s/$DB_NAME%s",
-        "driver" -> "org.postgresql.Driver",
-        "username" -> DB_USER,
-        "password" -> DB_PASSWORD,
-        "connectionPool" -> "HikariCP",
-        "numThreads" -> 5,
-        "poolSize" -> 20,
-        "keepAliveConnection" -> true,
+    mapAsJavaMap(
+      Map(
+        "postgres" -> Map(
+          "url" -> f"jdbc:postgresql://$DB_HOST%s:$DB_PORT%s/$DB_NAME%s",
+          "driver" -> "org.postgresql.Driver",
+          "username" -> DB_USER,
+          "password" -> DB_PASSWORD,
+          "connectionPool" -> "HikariCP",
+          "numThreads" -> 5,
+          "poolSize" -> 20,
+          "keepAliveConnection" -> true,
+        ),
       ),
-    ).asJava,
+    ),
   )
 
   def createDatabase(name: String = "postgres"): Database = {
