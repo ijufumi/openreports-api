32366fd419e933d8e30b46127f8e7ac728cedebb
Takafumi Iju <ijufumi@gmail.com>
2022-10-12 10:21:34 +0900

    Fixed error of map

--- src/main/scala/jp/ijufumi/openreports/database/DatabaseFactory.scala
+++ src/main/scala/jp/ijufumi/openreports/database/DatabaseFactory.scala
@@ -12,15 +12,17 @@ object DatabaseFactory {
   private val config = ConfigFactory.parseMap(
     mapAsJavaMap(
       Map(
-        "postgres" -> Map(
-          "url" -> f"jdbc:postgresql://$DB_HOST%s:$DB_PORT%s/$DB_NAME%s",
-          "driver" -> "org.postgresql.Driver",
-          "username" -> DB_USER,
-          "password" -> DB_PASSWORD,
-          "connectionPool" -> "HikariCP",
-          "numThreads" -> 5,
-          "poolSize" -> 20,
-          "keepAliveConnection" -> true,
+        "postgres" -> mapAsJavaMap(
+          Map(
+            "url" -> f"jdbc:postgresql://$DB_HOST%s:$DB_PORT%s/$DB_NAME%s",
+            "driver" -> "org.postgresql.Driver",
+            "username" -> DB_USER,
+            "password" -> DB_PASSWORD,
+            "connectionPool" -> "HikariCP",
+            "numThreads" -> 5,
+            "poolSize" -> 20,
+            "keepAliveConnection" -> true,
+          ),
         ),
       ),
     ),
