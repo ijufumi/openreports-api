10cf7a4cf6201d078a20a036a740e416938753b9
Takafumi Iju <ijufumi@gmail.com>
2022-10-21 15:29:19 +0900

    [WIP] workspaces on member.

--- src/main/scala/jp/ijufumi/openreports/vo/response/Member.scala
+++ src/main/scala/jp/ijufumi/openreports/vo/response/Member.scala
@@ -1,8 +1,17 @@
 package jp.ijufumi.openreports.vo.response
 
+import jp.ijufumi.openreports.entities.{Member => MemberEntity, Workspace => WorkspaceEntity}
+
 case class Member(
     id: String,
     email: String,
     name: String,
     apiToken: String = "",
+    workspaces: Seq[Workspace] = Seq.empty,
 )
+
+object Member {
+  def apply(member: MemberEntity, workspaces: Seq[WorkspaceEntity], apiToken: String): Member = {
+    Member(member.id, member.email, member.name, apiToken, workspaces.map(w => Workspace(w)))
+  }
+}
