20204fd88b06706087a3f9deaf3c708f175c0b69
Takafumi Iju <ijufumi@gmail.com>
2022-10-28 09:52:59 +0900

    Add storageType and fileSize to response

--- src/main/scala/jp/ijufumi/openreports/vo/response/ReportTemplate.scala
+++ src/main/scala/jp/ijufumi/openreports/vo/response/ReportTemplate.scala
@@ -1,17 +1,28 @@
 package jp.ijufumi.openreports.vo.response
 
+import jp.ijufumi.openreports.entities.enums.StorageTypes.StorageType
 import jp.ijufumi.openreports.entities.{ReportTemplate => ReportTemplateEntity}
 
 case class ReportTemplate(
     id: String,
     name: String,
     filePath: String,
+    storageType: StorageType,
+    fileSize: Long,
     createdAt: Long,
     updatedAt: Long,
 )
 
 object ReportTemplate {
   def apply(entity: ReportTemplateEntity): ReportTemplate = {
-    ReportTemplate(entity.id, entity.name, entity.filePath, entity.createdAt, entity.updatedAt)
+    ReportTemplate(
+      entity.id,
+      entity.name,
+      entity.filePath,
+      entity.storageType,
+      entity.fileSize,
+      entity.createdAt,
+      entity.updatedAt,
+    )
   }
 }
