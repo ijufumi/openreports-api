f2632d8ac65dcc30c6229a27641ff1f6f91f663d
Takafumi Iju <ijufumi@gmail.com>
2022-10-29 09:41:48 +0900

    Remove parentheses

--- src/main/scala/jp/ijufumi/openreports/repositories/db/DataSourceRepository.scala
+++ src/main/scala/jp/ijufumi/openreports/repositories/db/DataSourceRepository.scala
@@ -5,7 +5,7 @@ import jp.ijufumi.openreports.entities.{DataSource, DriverType}
 trait DataSourceRepository {
   def getById(id: String): Option[(DataSource, DriverType)]
 
-  def getAll(): Seq[DataSource]
+  def getAll: Seq[DataSource]
 
   def register(dataSource: DataSource): Option[(DataSource, DriverType)]
 
