0f8b656136f6f0380abf94f4180fd7cca8bfb759
Takafumi Iju <ijufumi@gmail.com>
2022-10-30 07:54:09 +0900

    Add BaseModule

---
+++ src/main/scala/jp/ijufumi/openreports/injector/BaseModule.scala
@@ -0,0 +1,18 @@
+package jp.ijufumi.openreports.injector
+
+import com.google.inject.{AbstractModule, Singleton}
+
+abstract class BaseModule extends AbstractModule {
+  override def configure(): Unit = {
+    super.configure()
+  }
+
+  def bindAsSingleton[T](
+      interfaceClass: java.lang.Class[T],
+      implementClass: java.lang.Class[_ <: T],
+  ) = {
+    bind(interfaceClass)
+      .to(implementClass)
+      .in(classOf[Singleton])
+  }
+}
--- src/main/scala/jp/ijufumi/openreports/injector/RepositoryModule.scala
+++ src/main/scala/jp/ijufumi/openreports/injector/RepositoryModule.scala
@@ -1,34 +1,20 @@
 package jp.ijufumi.openreports.injector
 
-import com.google.inject.{AbstractModule, Singleton}
 import jp.ijufumi.openreports.repositories.system._
 import jp.ijufumi.openreports.repositories.system.impl._
 import jp.ijufumi.openreports.repositories.db._
 import jp.ijufumi.openreports.repositories.db.impl._
 
-class RepositoryModule extends AbstractModule {
+class RepositoryModule extends BaseModule {
   override def configure(): Unit = {
     super.configure()
-    bind(classOf[MemberRepository])
-      .to(classOf[MemberRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[GoogleRepository])
-      .to(classOf[GoogleRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[WorkspaceRepository])
-      .to(classOf[WorkspaceRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[WorkspaceMemberRepository])
-      .to(classOf[WorkspaceMemberRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[DataSourceRepository])
-      .to(classOf[DataSourceRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[ReportRepository])
-      .to(classOf[ReportRepositoryImpl])
-      .in(classOf[Singleton])
-    bind(classOf[ReportTemplateRepository])
-      .to(classOf[ReportTemplateRepositoryImpl])
-      .in(classOf[Singleton])
+    bindAsSingleton(classOf[MemberRepository], classOf[MemberRepositoryImpl])
+    bindAsSingleton(classOf[GoogleRepository], classOf[GoogleRepositoryImpl])
+    bindAsSingleton(classOf[WorkspaceRepository], classOf[WorkspaceRepositoryImpl])
+    bindAsSingleton(classOf[WorkspaceMemberRepository], classOf[WorkspaceMemberRepositoryImpl])
+    bindAsSingleton(classOf[DataSourceRepository], classOf[DataSourceRepositoryImpl])
+    bindAsSingleton(classOf[ReportRepository], classOf[ReportRepositoryImpl])
+    bindAsSingleton(classOf[ReportTemplateRepository], classOf[ReportTemplateRepositoryImpl])
+    bindAsSingleton(classOf[StorageRepository], classOf[StorageRepositoryImpl])
   }
 }
