537c656c320e345cd6abccac05b3022205d2409e
Takafumi Iju <ijufumi@gmail.com>
2022-10-31 06:45:36 +0900

    Explicit the return type

--- src/main/scala/jp/ijufumi/openreports/injector/BaseModule.scala
+++ src/main/scala/jp/ijufumi/openreports/injector/BaseModule.scala
@@ -10,13 +10,13 @@ abstract class BaseModule extends AbstractModule {
   def bindAsSingleton[T](
       interfaceClass: java.lang.Class[T],
       implementClass: java.lang.Class[_ <: T],
-  ) = {
+  ): Unit = {
     bind(interfaceClass)
       .to(implementClass)
       .in(classOf[Singleton])
   }
 
-  def bindInstance[T](interfaceClass: java.lang.Class[T], instance: T) = {
+  def bindInstance[T](interfaceClass: java.lang.Class[T], instance: T): Unit = {
     bind(interfaceClass).toInstance(instance)
   }
 }
