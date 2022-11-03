06e97f4d0907360792aa50792f16b1a15fe7165e
Takafumi Iju <ijufumi@gmail.com>
2022-11-04 06:34:07 +0900

    [WIP] s3 bucket

--- src/main/scala/jp/ijufumi/openreports/repositories/system/impl/AwsS3RepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/repositories/system/impl/AwsS3RepositoryImpl.scala
@@ -1,17 +1,24 @@
 package jp.ijufumi.openreports.repositories.system.impl
 
 import com.google.inject.Inject
+import jp.ijufumi.openreports.entities.Storage
 import jp.ijufumi.openreports.repositories.system.AwsS3Repository
 import jp.ijufumi.openreports.repositories.db.StorageRepository
 import jp.ijufumi.openreports.exceptions.NotFoundException
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCredentialsProvider}
 import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.model._
 
 import java.io.InputStream
 
 class AwsS3RepositoryImpl @Inject() (storageRepository: StorageRepository) extends AwsS3Repository {
-  override def get(workspaceId: String, key: String): InputStream = ???
+  override def get(workspaceId: String, key: String): InputStream = {
+    val storage = this.getStorage(workspaceId)
+    val client = this.createClient(storage)
+    val request = GetObjectRequest.builder().bucket(storage.s3BucketName).key(key).build()
+    client.getObject(request)
+  }
 
   override def create(workspaceId: String, key: String, file: InputStream): Unit = ???
 
@@ -19,12 +26,15 @@ class AwsS3RepositoryImpl @Inject() (storageRepository: StorageRepository) exten
 
   override def url(workspaceId: String, key: String): String = ???
 
-  private def client(workspaceId: String): S3Client = {
+  private def getStorage(workspaceId: String): Storage = {
     val storageList = storageRepository.gets(workspaceId)
     if (storageList.isEmpty) {
       throw new NotFoundException(s"storage doesn't exist. workspaceId: ${workspaceId}")
     }
-    val storage = storageList.head
+    storageList.head
+  }
+
+  private def createClient(storage: Storage): S3Client = {
     val credentials = AwsBasicCredentials.create(storage.awsAccessKey, storage.awsSecretKeyId)
     val region = Region.of(storage.awsRegion)
     S3Client
