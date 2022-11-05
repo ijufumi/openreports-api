640730f028d302f2cff0caacdc22368d5fad3657
Takafumi Iju <ijufumi@gmail.com>
2022-11-05 14:26:12 +0900

    Change to be able to output report without data source.

--- src/main/scala/jp/ijufumi/openreports/services/impl/OutputServiceImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/services/impl/OutputServiceImpl.scala
@@ -3,8 +3,7 @@ package jp.ijufumi.openreports.services.impl
 import com.google.inject.Inject
 import jp.ijufumi.openreports.config.Config
 import jp.ijufumi.openreports.entities.enums.StorageTypes.StorageType
-import jp.ijufumi.openreports.repositories.system.LocalFileRepository
-import jp.ijufumi.openreports.services.{DataSourceService, StorageService, OutputService}
+import jp.ijufumi.openreports.services.{DataSourceService, OutputService, StorageService}
 import jp.ijufumi.openreports.utils.{Dates, Logging}
 
 import scala.util.Using
@@ -13,12 +12,12 @@ import org.jxls.jdbc.JdbcHelper
 import org.jxls.util.JxlsHelper
 
 import java.io.File
-import java.nio.file.{Files, FileSystems}
+import java.nio.file.{Files, FileSystems, Path}
 import java.time.LocalDateTime
 
 class OutputServiceImpl @Inject() (
-                                    dataSourceService: DataSourceService,
-                                    fileService: StorageService,
+    dataSourceService: DataSourceService,
+    storageService: StorageService,
 ) extends OutputService
     with Logging {
   override def output(
@@ -41,18 +40,42 @@ class OutputServiceImpl @Inject() (
       Files.createDirectories(outputDirectory)
     }
 
+    if (dataSourceId == null) {
+      this.output(workspaceId, filePath, storageType, outputFile)
+    } else {
+      this.outputWithDataSource(workspaceId, filePath, storageType, dataSourceId, outputFile)
+    }
+
+    Some(outputFile.toFile)
+  }
+
+  private def outputWithDataSource(
+      workspaceId: String,
+      filePath: String,
+      storageType: StorageType,
+      dataSourceId: String,
+      outputFile: Path,
+  ): Unit = {
     Using(dataSourceService.connection(dataSourceId)) { conn =>
       val jdbcHelper = new JdbcHelper(conn)
       val context = new Context()
       context.putVar("conn", conn)
       context.putVar("jdbc", jdbcHelper)
-      Using(fileService.get(workspaceId, filePath, storageType)) { inputs =>
-        Using(Files.newOutputStream(outputFile)) { outputs =>
-          JxlsHelper.getInstance().processTemplate(inputs, outputs, context)
-        }
+      this.output(workspaceId, filePath, storageType, outputFile, context)
+    }
+  }
+
+  private def output(
+      workspaceId: String,
+      filePath: String,
+      storageType: StorageType,
+      outputFile: Path,
+      context: Context = new Context(),
+  ): Unit = {
+    Using(storageService.get(workspaceId, filePath, storageType)) { inputs =>
+      Using(Files.newOutputStream(outputFile)) { outputs =>
+        JxlsHelper.getInstance().processTemplate(inputs, outputs, context)
       }
     }
-
-    Some(outputFile.toFile)
   }
 }
