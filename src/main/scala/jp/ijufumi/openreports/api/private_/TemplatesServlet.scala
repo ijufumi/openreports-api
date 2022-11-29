0ebccfb61909b222f4d525a9bcc88c135e80e787
Takafumi Iju <ijufumi@gmail.com>
2022-11-30 07:34:14 +0900

    Modify to get request from form.

--- src/main/scala/jp/ijufumi/openreports/api/private_/TemplatesServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/private_/TemplatesServlet.scala
@@ -1,6 +1,7 @@
 package jp.ijufumi.openreports.api.private_
 
 import com.google.inject.Inject
+import org.scalatra.forms._
 import jp.ijufumi.openreports.api.base.PrivateAPIServletBase
 import jp.ijufumi.openreports.services.{LoginService, ReportService}
 import jp.ijufumi.openreports.vo.request.CreateTemplate
@@ -15,14 +16,24 @@ class TemplatesServlet @Inject() (loginService: LoginService, reportService: Rep
   }
 
   post("/") {
-    val _workspaceId = workspaceId()
-    val file = fileParams("file")
-    val req = extractBody[CreateTemplate]()
-    val res = reportService.createTemplate(_workspaceId, req, file)
-    if (res.isEmpty) {
-      badRequest("something wrong...")
-    } else {
-      ok(res.get)
-    }
+    val postForm = mapping(
+      "name" -> text(),
+    )(CreateTemplate.apply)
+
+    validate(postForm)(
+      (errors) => {
+        badRequest(errors)
+      },
+      (form: CreateTemplate) => {
+        val _workspaceId = workspaceId()
+        val file = fileParams("file")
+        val res = reportService.createTemplate(_workspaceId, form, file)
+        if (res.isEmpty) {
+          badRequest("something wrong...")
+        } else {
+          ok(res.get)
+        }
+      },
+    )
   }
 }
