423fe1fefc0a8f35559e9dd66f57352a57c95e11
Takafumi Iju <ijufumi@gmail.com>
2023-01-08 08:52:52 +0900

    Fixed compile error

--- src/main/scala/jp/ijufumi/openreports/entities/Report.scala
+++ src/main/scala/jp/ijufumi/openreports/entities/Report.scala
@@ -1,7 +1,6 @@
 package jp.ijufumi.openreports.entities
 
 import slick.jdbc.PostgresProfile.api._
-import jp.ijufumi.openreports.gateways.datastores.database.repositories.impl.queries.templateQuery
 import jp.ijufumi.openreports.utils.Dates
 
 case class Report(
@@ -28,8 +27,6 @@ class Reports(tag: Tag)
   def createdAt = column[Long]("created_at")
   def updatedAt = column[Long]("updated_at")
   def versions = column[Long]("versions")
-
-  def template = foreignKey("templates", templateId, templateQuery)(_.id)
 
   override def * =
     (
