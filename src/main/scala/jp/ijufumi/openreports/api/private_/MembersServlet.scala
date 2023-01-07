68ce9c53896f171c57d19e7dad327db7d9cd693c
Takafumi Iju <ijufumi@gmail.com>
2023-01-07 12:58:14 +0900

    Delete unnecessary import

--- src/main/scala/jp/ijufumi/openreports/api/private_/MembersServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/api/private_/MembersServlet.scala
@@ -3,7 +3,6 @@ package jp.ijufumi.openreports.api.private_
 import com.google.inject.Inject
 import jp.ijufumi.openreports.api.base.PrivateAPIServletBase
 import jp.ijufumi.openreports.services.LoginService
-import org.scalatra._
 
 class MembersServlet @Inject() (loginService: LoginService)
     extends PrivateAPIServletBase(loginService) {
