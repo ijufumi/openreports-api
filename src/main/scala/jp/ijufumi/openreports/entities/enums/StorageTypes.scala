fd548e4de1855a7a67e73b9f890013cf8fcd968f
Takafumi Iju <ijufumi@gmail.com>
2023-01-09 10:06:40 +0900

    Formatting

--- src/main/scala/jp/ijufumi/openreports/entities/enums/StorageTypes.scala
+++ src/main/scala/jp/ijufumi/openreports/entities/enums/StorageTypes.scala
@@ -1,15 +1,18 @@
 package jp.ijufumi.openreports.entities.enums
 
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
 import slick.jdbc.PostgresProfile.api._
 
 object StorageTypes extends Enumeration {
   type StorageType = Value
 
-  val Local = Value("local")
-  val S3 = Value("s3")
+  val Local: StorageTypes.Value = Value("local")
+  val S3: StorageTypes.Value = Value("s3")
 
-  implicit val storageTypeMapper = MappedColumnType.base[StorageTypes.StorageType, String](
-    e => e.toString,
-    s => StorageTypes.withName(s)
-  )
+  implicit val storageTypeMapper: JdbcType[StorageType] with BaseTypedType[StorageType] =
+    MappedColumnType.base[StorageTypes.StorageType, String](
+      e => e.toString,
+      s => StorageTypes.withName(s),
+    )
 }
