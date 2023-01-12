9087250da344a6f16619102f8264785c0ba87a7c
Takafumi Iju <ijufumi@gmail.com>
2023-01-13 06:53:20 +0900

    Add data source process

--- src/main/scala/jp/ijufumi/openreports/apis/private_/DataSourceServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/apis/private_/DataSourceServlet.scala
@@ -5,7 +5,7 @@ import jp.ijufumi.openreports.apis.base.PrivateAPIServletBase
 import jp.ijufumi.openreports.services.{DataSourceService, LoginService}
 import jp.ijufumi.openreports.models.inputs.{CreateDataSource, UpdateDataSource}
 
-class DataSourceServlet @Inject()(
+class DataSourceServlet @Inject() (
     loginService: LoginService,
     dataSourceService: DataSourceService,
 ) extends PrivateAPIServletBase(loginService) {
@@ -17,14 +17,43 @@ class DataSourceServlet @Inject()(
   }
 
   post("/") {
+    val _workspaceId = workspaceId()
     val requestVal = extractBody[CreateDataSource]()
+    val result = dataSourceService.registerDataSource(_workspaceId, requestVal)
+    if (result.isEmpty) {
+      badRequest("something wrong")
+    } else {
+      ok(result)
+    }
   }
 
   put("/:id") {
+    val _workspaceId = workspaceId()
+    val id = params("id")
     val requestVal = extractBody[UpdateDataSource]()
+    val result = dataSourceService.updateDataSource(_workspaceId, id, requestVal)
+    if (result.isEmpty) {
+      badRequest("something wrong")
+    } else {
+      ok(result)
+    }
   }
 
-  get("/:id") {}
+  get("/:id") {
+    val _workspaceId = workspaceId()
+    val id = params("id")
+    val result = dataSourceService.getDataSource(_workspaceId, id)
+    if (result.isEmpty) {
+      notFound("doesn't exist")
+    } else {
+      ok(result)
+    }
+  }
 
-  delete("/:id") {}
+  delete("/:id") {
+    val _workspaceId = workspaceId()
+    val id = params("id")
+    dataSourceService.deleteDataSource(_workspaceId, id)
+    ok("success")
+  }
 }
