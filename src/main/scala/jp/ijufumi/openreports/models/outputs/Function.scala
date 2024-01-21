3efb912bd2c787843197c72b167e9293f3b4e687
Takafumi Iju <ijufumi@gmail.com>
2024-01-21 17:14:44 +0900

    feat: add enum of action

--- src/main/scala/jp/ijufumi/openreports/models/outputs/Function.scala
+++ src/main/scala/jp/ijufumi/openreports/models/outputs/Function.scala
@@ -1,8 +1,9 @@
 package jp.ijufumi.openreports.models.outputs
 
 import jp.ijufumi.openreports.gateways.datastores.database.entities.{Function => FunctionEntity}
+import jp.ijufumi.openreports.models.value.enums.ActionTypes.ActionType
 
-case class Function(resource: String, action: String)
+case class Function(resource: String, action: ActionType)
 
 object Function {
   def apply(entity: FunctionEntity): Function = {
