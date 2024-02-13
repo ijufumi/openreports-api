9e10eb48afdeee651a6fe564a4eea7b9ba6ece5e
Takafumi Iju <ijufumi@gmail.com>
2024-02-14 08:40:29 +0900

    feat: change data source repository

--- src/main/scala/jp/ijufumi/openreports/gateways/datastores/database/repositories/impl/DataSourceRepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/gateways/datastores/database/repositories/impl/DataSourceRepositoryImpl.scala
@@ -1,7 +1,7 @@
 package jp.ijufumi.openreports.gateways.datastores.database.repositories.impl
 
 import com.google.inject.Inject
-import jp.ijufumi.openreports.gateways.datastores.database.entities.{DataSource, DriverType}
+import jp.ijufumi.openreports.models.outputs.{DataSource, DriverType}
 import queries.{dataSourceQuery => query, driverTypeQuery}
 import jp.ijufumi.openreports.gateways.datastores.database.repositories.DataSourceRepository
 import slick.jdbc.JdbcBackend.Database
@@ -15,19 +15,19 @@ class DataSourceRepositoryImpl @Inject() (db: Database) extends DataSourceReposi
       .filter(_.id === id)
       .filter(_.workspaceId === workspaceId)
     val dataSources = Await.result(db.run(getDataSources.result), queryTimeout)
-    Some(dataSources.head)
+    Some(dataSources.head).map(d => DataSource(d))
   }
 
   override def getAll(workspaceId: String): Seq[DataSource] = {
     val getDataSources = query
       .filter(_.workspaceId === workspaceId)
-    Await.result(db.run(getDataSources.result), queryTimeout)
+    Await.result(db.run(getDataSources.result), queryTimeout).map(d => DataSource(d))
   }
 
   override def getByIdWithDriverType(
       workspaceId: String,
       id: String,
-  ): Option[(DataSource, DriverType)] = {
+  ): Option[DataSource] = {
     val getDataSources = query
       .join(driverTypeQuery)
       .on(_.driverTypeId === _.id)
@@ -37,24 +37,24 @@ class DataSourceRepositoryImpl @Inject() (db: Database) extends DataSourceReposi
     if (dataSources.isEmpty) {
       return None
     }
-    Some(dataSources.head)
+    Some(dataSources.head).map(d => DataSource(d._1, d._2))
   }
 
-  override def getAllWithDriverType(workspaceId: String): Seq[(DataSource, DriverType)] = {
+  override def getAllWithDriverType(workspaceId: String): Seq[DataSource] = {
     val getDataSources = query
       .join(driverTypeQuery)
       .on(_.driverTypeId === _.id)
       .filter(_._1.workspaceId === workspaceId)
-    Await.result(db.run(getDataSources.result), queryTimeout)
+    Await.result(db.run(getDataSources.result), queryTimeout).map(d => DataSource(d._1, d._2))
   }
 
   override def register(dataSource: DataSource): Option[DataSource] = {
-    Await.result(db.run((query += dataSource).withPinnedSession), queryTimeout)
+    Await.result(db.run((query += dataSource.toEntity).withPinnedSession), queryTimeout)
     getById(dataSource.workspaceId, dataSource.id)
   }
 
   override def update(dataSource: DataSource): Unit = {
-    query.insertOrUpdate(dataSource).withPinnedSession
+    query.insertOrUpdate(dataSource.toEntity).withPinnedSession
   }
 
   override def delete(workspaceId: String, id: String): Unit = {
