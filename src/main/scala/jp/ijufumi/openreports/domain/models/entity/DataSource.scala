9a1c662c54df684cde797a1ab817bdb1f0d35947
Takafumi Iju <ijufumi@gmail.com>
2024-03-03 09:59:44 +0900

    feat: change to use implicit conversion of driver type
    fixed build error

--- src/main/scala/jp/ijufumi/openreports/domain/models/entity/DataSource.scala
+++ src/main/scala/jp/ijufumi/openreports/domain/models/entity/DataSource.scala
@@ -7,6 +7,7 @@ import jp.ijufumi.openreports.infrastructure.datastores.database.entities.{
 import jp.ijufumi.openreports.presentation.models.requests.UpdateDataSource
 import jp.ijufumi.openreports.utils.Dates
 import jp.ijufumi.openreports.presentation.models.responses.{DataSource => DataSourceResponse}
+import scala.language.implicitConversions
 import jp.ijufumi.openreports.domain.models.entity.DriverType.conversions._
 
 case class DataSource(
@@ -84,8 +85,6 @@ object DataSource {
     )
   }
   object conversions {
-    import scala.language.implicitConversions
-
     implicit def toEntity(model: DataSource): DataSourceEntity = {
       model.toEntity
     }
--- src/main/scala/jp/ijufumi/openreports/domain/models/entity/DriverType.scala
+++ src/main/scala/jp/ijufumi/openreports/domain/models/entity/DriverType.scala
@@ -5,6 +5,7 @@ import jp.ijufumi.openreports.infrastructure.datastores.database.entities.{
   DriverType => DriverTypeEntity,
 }
 import jp.ijufumi.openreports.presentation.models.responses.{DriverType => DriverTypeResponse}
+import scala.language.implicitConversions
 
 case class DriverType(
     id: String,
@@ -26,8 +27,6 @@ object DriverType {
   }
 
   object conversions {
-    import scala.language.implicitConversions
-
     implicit def toResponse(model: DriverType): DriverTypeResponse = {
       model.toResponse
     }
