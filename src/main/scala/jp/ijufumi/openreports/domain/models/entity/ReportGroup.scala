e7551b7bf668fd3a9bd0725227fd74a47a027cc1
Takafumi Iju <ijufumi@gmail.com>
2024-03-06 07:49:45 +0900

    feat: change to use implicit conversion of report group

--- src/main/scala/jp/ijufumi/openreports/domain/models/entity/ReportGroup.scala
+++ src/main/scala/jp/ijufumi/openreports/domain/models/entity/ReportGroup.scala
@@ -1,6 +1,8 @@
 package jp.ijufumi.openreports.domain.models.entity
 
-import jp.ijufumi.openreports.infrastructure.datastores.database.entities.{ReportGroup => ReportGroupEntity}
+import jp.ijufumi.openreports.infrastructure.datastores.database.entities.{
+  ReportGroup => ReportGroupEntity,
+}
 import jp.ijufumi.openreports.presentation.models.requests.UpdateReportGroup
 import jp.ijufumi.openreports.utils.Dates
 import jp.ijufumi.openreports.presentation.models.responses.{ReportGroup => ReportGroupResponse}
@@ -44,5 +46,42 @@ object ReportGroup {
       entity.updatedAt,
       entity.versions,
     )
+  }
+
+  object conversions {
+    import scala.language.implicitConversions
+
+    implicit def toReportGroupEntity(model: ReportGroup): ReportGroupEntity = {
+      model.toEntity
+    }
+
+    implicit def toReportGroupEntities(model: Seq[ReportGroup]): Seq[ReportGroupEntity] = {
+      model.map(m => m.toEntity)
+    }
+
+    implicit def fromReportGroupEntity(entity: ReportGroupEntity): ReportGroup = {
+      ReportGroup(entity)
+    }
+
+    implicit def fromReportGroupEntity2(entity: Option[ReportGroupEntity]): Option[ReportGroup] = {
+      entity.map(e => ReportGroup(e))
+    }
+
+    implicit def fromReportGroupEntities(entity: Seq[ReportGroupEntity]): Seq[ReportGroup] = {
+      entity.map(e => ReportGroup(e))
+    }
+
+    implicit def toReportGroupResponse(model: ReportGroup): ReportGroupResponse = {
+      model.toResponse
+    }
+
+    implicit def toReportGroupResponse2(model: Option[ReportGroup]): Option[ReportGroupResponse] = {
+      model.map(m => m.toResponse)
+    }
+
+    implicit def toReportGroupResponses(model: Seq[ReportGroup]): Seq[ReportGroupResponse] = {
+      model.map(m => m.toResponse)
+    }
+
   }
 }
