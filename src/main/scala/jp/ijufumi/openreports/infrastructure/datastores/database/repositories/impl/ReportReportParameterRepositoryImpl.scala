e1d6f102a18cfcc4f1734c1ecc5266de0d36ea39
Takafumi Iju <ijufumi@gmail.com>
2024-03-22 06:59:30 +0900

    feat: Add db as parameter to report report parameter repository

--- src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/impl/ReportReportParameterRepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/impl/ReportReportParameterRepositoryImpl.scala
@@ -13,9 +13,9 @@ import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Await
 
-class ReportReportParameterRepositoryImpl @Inject() (db: Database)
-    extends ReportReportParameterRepository {
+class ReportReportParameterRepositoryImpl extends ReportReportParameterRepository {
   override def gets(
+      db: Database,
       offset: Int = 0,
       limit: Int = -1,
   ): (Seq[ReportReportParameter], Int) = {
@@ -27,7 +27,7 @@ class ReportReportParameterRepositoryImpl @Inject() (db: Database)
     (Await.result(db.run(filtered.result), queryTimeout).map(r => ReportReportParameter(r)), count)
   }
 
-  override def getById(id: String): Option[ReportReportParameter] = {
+  override def getById(db: Database, id: String): Option[ReportReportParameter] = {
     val getById = query
       .filter(_.id === id)
     val models = Await.result(db.run(getById.result), queryTimeout)
@@ -37,44 +37,50 @@ class ReportReportParameterRepositoryImpl @Inject() (db: Database)
     Some(models.head).map(r => ReportReportParameter(r))
   }
 
-  override def getByIds(ids: Seq[String]): Seq[ReportReportParameter] = {
+  override def getByIds(db: Database, ids: Seq[String]): Seq[ReportReportParameter] = {
     val getById = query
       .filter(_.id.inSet(ids))
     Await.result(db.run(getById.result), queryTimeout).map(r => ReportReportParameter(r))
   }
 
-  override def register(model: ReportReportParameter): Option[ReportReportParameter] = {
+  override def register(
+      db: Database,
+      model: ReportReportParameter,
+  ): Option[ReportReportParameter] = {
     val register = (query += model).withPinnedSession
     Await.result(db.run(register), queryTimeout)
-    getById(model.id)
+    getById(db, model.id)
   }
 
-  override def registerInBatch(model: Seq[ReportReportParameter]): Seq[ReportReportParameter] = {
+  override def registerInBatch(
+      db: Database,
+      model: Seq[ReportReportParameter],
+  ): Seq[ReportReportParameter] = {
     val register = (query ++= model).withPinnedSession
     Await.result(db.run(register), queryTimeout)
     val ids = model.map(m => m.id)
-    getByIds(ids)
+    getByIds(db, ids)
   }
 
-  override def update(model: ReportReportParameter): Unit = {
+  override def update(db: Database, model: ReportReportParameter): Unit = {
     val newModel = model.copy(updatedAt = Dates.currentTimestamp())
     val updateQuery = query.insertOrUpdate(newModel).withPinnedSession
     Await.result(db.run(updateQuery), queryTimeout)
   }
 
-  override def delete(id: String): Unit = {
+  override def delete(db: Database, id: String): Unit = {
     val getById = query
       .filter(_.id === id)
     Await.result(db.run(getById.delete), queryTimeout)
   }
 
-  override def deleteByReportId(id: String): Unit = {
+  override def deleteByReportId(db: Database, id: String): Unit = {
     val getById = query
       .filter(_.reportId === id)
     Await.result(db.run(getById.delete), queryTimeout)
   }
 
-  override def deleteByReportParameterId(id: String): Unit = {
+  override def deleteByReportParameterId(db: Database, id: String): Unit = {
     val getById = query
       .filter(_.reportParameterId === id)
     Await.result(db.run(getById.delete), queryTimeout)
