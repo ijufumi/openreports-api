9c190e4f93fa81464446f38d0a92c52c43f8571c
Takafumi Iju <ijufumi@gmail.com>
2024-03-24 10:29:34 +0900

    feat: Add db as parameter to role function repository

--- src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/RoleFunctionRepository.scala
+++ src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/RoleFunctionRepository.scala
@@ -1,12 +1,14 @@
 package jp.ijufumi.openreports.infrastructure.datastores.database.repositories
 
 import jp.ijufumi.openreports.domain.models.entity.RoleFunction
+import slick.jdbc.JdbcBackend.Database
 
 trait RoleFunctionRepository {
   def getAll(
+      db: Database,
   ): Seq[RoleFunction]
 
-  def getByRoleId(roleId: String): Seq[RoleFunction]
+  def getByRoleId(db: Database,roleId: String): Seq[RoleFunction]
 
-  def getByFunctionId(functionId: String): Seq[RoleFunction]
+  def getByFunctionId(db: Database,functionId: String): Seq[RoleFunction]
 }
--- src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/impl/RoleFunctionRepositoryImpl.scala
+++ src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/database/repositories/impl/RoleFunctionRepositoryImpl.scala
@@ -1,6 +1,5 @@
 package jp.ijufumi.openreports.infrastructure.datastores.database.repositories.impl
 
-import com.google.inject.Inject
 import jp.ijufumi.openreports.infrastructure.datastores.database.repositories.RoleFunctionRepository
 import jp.ijufumi.openreports.infrastructure.datastores.database.repositories.impl.queries.{
   roleFunctionQuery => query,
@@ -12,19 +11,19 @@ import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Await
 
-class RoleFunctionRepositoryImpl @Inject() (db: Database) extends RoleFunctionRepository {
-  override def getAll(): Seq[RoleFunction] = {
+class RoleFunctionRepositoryImpl extends RoleFunctionRepository {
+  override def getAll(db: Database): Seq[RoleFunction] = {
     val result = Await.result(db.run(query.result), queryTimeout)
     result
   }
 
-  override def getByRoleId(roleId: String): Seq[RoleFunction] = {
+  override def getByRoleId(db: Database, roleId: String): Seq[RoleFunction] = {
     val result = Await
       .result(db.run(query.filter(_.roleId === roleId).result), queryTimeout)
     result
   }
 
-  override def getByFunctionId(functionId: String): Seq[RoleFunction] = {
+  override def getByFunctionId(db: Database, functionId: String): Seq[RoleFunction] = {
     val result = Await
       .result(db.run(query.filter(_.functionId === functionId).result), queryTimeout)
     result
