d6a52a3e54eba93951617d1981aacebb41d9e322
Takafumi Iju <ijufumi@gmail.com>
2024-04-19 06:54:21 +0900

    fix: Set apiToken when login

--- src/main/scala/jp/ijufumi/openreports/presentation/controllers/public_/LoginServlet.scala
+++ src/main/scala/jp/ijufumi/openreports/presentation/controllers/public_/LoginServlet.scala
@@ -1,9 +1,10 @@
 package jp.ijufumi.openreports.presentation.controllers.public_
 
 import com.google.inject.Inject
+import jp.ijufumi.openreports.configs.Config
 import jp.ijufumi.openreports.presentation.controllers.base.APIServletBase
 import jp.ijufumi.openreports.presentation.models.requests.{GoogleLogin, Login}
-import jp.ijufumi.openreports.presentation.models.responses.GoogleAuthUrl
+import jp.ijufumi.openreports.presentation.models.responses.{GoogleAuthUrl, Member}
 import jp.ijufumi.openreports.services.LoginService
 
 class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase {
@@ -13,6 +14,7 @@ class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase
     if (member.isEmpty) {
       unauthorized("email or password or both are incorrect")
     } else {
+      generateToken(member)
       ok(member.get)
     }
   }
@@ -26,7 +28,14 @@ class LoginServlet @Inject() (loginService: LoginService) extends APIServletBase
     if (member.isEmpty) {
       unauthorized("state or code or both are invalid")
     } else {
+      generateToken(member)
       ok(member.get)
     }
+  }
+
+  private def generateToken(member: Option[Member]): Unit = {
+    val apiToken = loginService.generateApiToken(member.get.id)
+    request.removeAttribute("member")
+    response.setHeader(Config.API_TOKEN_HEADER, apiToken)
   }
 }
