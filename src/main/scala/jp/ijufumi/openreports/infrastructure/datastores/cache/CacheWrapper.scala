8cf97040f68527fec39663dbcc56b4081dfbf962
Takafumi Iju <ijufumi@gmail.com>
2024-06-19 07:18:33 +0900

    fix: compile error

--- src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/cache/CacheWrapper.scala
+++ src/main/scala/jp/ijufumi/openreports/infrastructure/datastores/cache/CacheWrapper.scala
@@ -3,19 +3,20 @@ package jp.ijufumi.openreports.infrastructure.datastores.cache
 import jp.ijufumi.openreports.configs.Config
 import scalacache._
 import scalacache.modes.try_._
-import scalacache.redis.RedisCache
+import scalacache.redis._
+import scalacache.serialization.binary._
 
 import scala.concurrent.duration._
 
 class CacheWrapper {
   private val defaultTtl = Config.CACHE_TTL_SEC
-  private val cache: Cache[Any] = RedisCache[Any](Config.REDIS_HOST, Config.REDIS_PORT)
+  implicit val redisCache: Cache[Any] = RedisCache[Any](Config.REDIS_HOST, Config.REDIS_PORT)
   private val lock = new java.util.concurrent.locks.ReentrantReadWriteLock()
 
   def put[T](cacheKey: CacheKey, value: T, args: String*)(implicit ttl: Long = defaultTtl): Unit = {
     try {
       lock.writeLock().lock()
-      cache.put(cacheKey.key(args: _*))(value, ttl = Some(Duration(ttl, SECONDS)))
+      redisCache.put(cacheKey.key(args: _*))(value, ttl = Some(Duration(ttl, SECONDS)))
     } finally {
       lock.writeLock().unlock()
     }
@@ -24,7 +25,7 @@ class CacheWrapper {
   def get[T](cacheKey: CacheKey, args: String*): Option[T] = {
     try {
       lock.readLock().lock()
-      val value = cache.get(cacheKey.key(args: _*))
+      val value = redisCache.get(cacheKey.key(args: _*))
       if (value.isFailure || value.get.isEmpty) {
         return None
       }
@@ -38,7 +39,7 @@ class CacheWrapper {
   def getAsSeq[T](cacheKey: CacheKey, args: String*): Option[Seq[T]] = {
     try {
       lock.readLock().lock()
-      val value = cache.get(cacheKey.key(args: _*))
+      val value = redisCache.get(cacheKey.key(args: _*))
       if (value.isFailure || value.get.isEmpty) {
         return None
       }
