603fbcfe1f3489082731297aea38ecb0ce8352cc
Takafumi Iju <ijufumi@gmail.com>
2026-03-27 11:51:02 +0900

    chore: パスワードチェックの説明を追加

--- src/main/scala/jp/ijufumi/openreports/presentation/request/UpdateMember.scala
+++ src/main/scala/jp/ijufumi/openreports/presentation/request/UpdateMember.scala
@@ -1,14 +1,15 @@
 package jp.ijufumi.openreports.presentation.request
 
-import jp.ijufumi.openreports.presentation.validation.Validator
+import jp.ijufumi.openreports.presentation.validation.{ValidationResult, Validator}
 
 case class UpdateMember(name: String, password: String)
 
 object UpdateMember {
   implicit val validate: Validator[UpdateMember] = new Validator[UpdateMember] {
-    def validate(param: UpdateMember) = collectViolations(
+    def validate(param: UpdateMember): ValidationResult = collectViolations(
       notEmpty("name", param.name),
       between("name", param.name.length, 1, 255),
+      // notEmpty("password", param.password), パスワード変更は任意項目のため、空文字チェックは不要です
     )
   }
 }
